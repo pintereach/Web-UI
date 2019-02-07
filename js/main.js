2a0d0b42a345589801f46508f11afab765dcf294
larynab <fsovreign@gmail.com>
2019-02-06 17:43:26 -0800

    arketing-page-alpha

--- js/main.js
+++ js/main.js
@@ -8,8 +8,35 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 
 menuButton.addEventListener('click', toggleMenu);
-
 //
 
 //images
 
+//
+
+//global
+const pAlla = document.querySelector(".content");
+	pAlla.addEventListener("mouseenter", function( event ) {   
+    event.target.style.color = "orange";
+    setTimeout(function() {
+      event.target.style.color = "#3d4449";
+      }, 2000);
+    event.stopPropagation();  
+  }, false);
+const pAllb = document.querySelector(".features");
+	pAllb.addEventListener("mouseenter", function( event ) {   
+    event.target.style.color = "orange";
+    setTimeout(function() {
+      event.target.style.color = "#3d4449";
+      }, 2000);
+    event.stopPropagation();  
+  }, false);
+const pAllc = document.querySelector(".posts");
+	pAllc.addEventListener("mouseenter", function( event ) {   
+    event.target.style.color = "orange";
+    setTimeout(function() {
+      event.target.style.color = "#3d4449";
+      }, 2000);
+    event.stopPropagation();  
+  }, false);
+//
