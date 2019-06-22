065368d2a656829f468439027dc140f6e150a5ac
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-06-22 11:53:22 -0700

    testing

--- js/global.js
+++ js/global.js
@@ -1,9 +1,10 @@
 //menu
 const toggleMenu = () => {
-	menu.classList.toggle('menu--open');
+	menu.classList.toggsdfsfsdfsdfle('menu--open');
 };
 const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
+const kdefjoiahdfhudgafg
 
 menuButton.addEventListener('click', toggleMenu);
 //
